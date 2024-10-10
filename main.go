27973399187170b55745fe650e37d5b32af09aaf
= <=>
2024-10-10 16:59:14 +0700

    database version

--- main.go
+++ main.go
@@ -1,17 +1,92 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "./webboard.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	r := gin.Default()
 
 	r.StaticFile("/webboard.html", "./webboard.html")
+
+	r.GET("/api/boards", func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c.Request.Context(), 3*time.Second)
+		defer cancel()
+
+		rows, err := db.QueryContext(ctx, "SELECT name, message FROM webboard")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		tables := []board{}
+		for rows.Next() {
+			var name, message string
+			if err := rows.Scan(&name, &message); err != nil {
+				slog.Error(err.Error())
+			}
+			tables = append(tables, board{Name: name, Message: message})
+		}
+		c.JSON(http.StatusOK, tables)
+	})
+	r.POST("/api/boards", func(c *gin.Context) {
+		var question board
+		if err := c.ShouldBindJSON(&question); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(c.Request.Context(), 3*time.Second)
+		defer cancel()
+
+		_, err = db.ExecContext(ctx, "INSERT INTO webboard(name,message) VALUES(?,?)", question.Name, question.Message)
+		if err != nil {
+			slog.Error(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		rows, err := db.QueryContext(ctx, "SELECT name, message FROM webboard")
+		if err != nil {
+			slog.Error(err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		tables := []board{}
+		for rows.Next() {
+			var name, message string
+			if err := rows.Scan(&name, &message); err != nil {
+				slog.Error(err.Error())
+			}
+			tables = append(tables, board{Name: name, Message: message})
+		}
+		c.JSON(http.StatusOK, tables)
+	})
 
 	fmt.Println("listening and serving on :", os.Getenv("PORT"))
 	r.Run()
